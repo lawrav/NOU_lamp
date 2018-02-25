4132eda43f3c3c9a55778c663f1f1da70eae66d0
LawraV <33949441+lawrav@users.noreply.github.com>
2018-02-25 16:29:54 -0500

    Tweet_Lamp
    This code is a basic blink mixed with sparkfun's electric imp

---
+++ Nou_Lamp_Arduino/Nou_Lamp_Arduino.ino
@@ -0,0 +1,37 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 12;
+int tweet = 9;
+int receivedTweet;
+// the setup routine runs once when you press reset:
+void setup() {                
+  Serial.begin(9600);
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  pinMode(tweet, INPUT);  
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  receivedTweet =  digitalRead(tweet);
+  Serial.println(receivedTweet);
+  if (receivedTweet == LOW )
+  {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);               // wait for a second
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);               // wait for a second
+  }
+}
